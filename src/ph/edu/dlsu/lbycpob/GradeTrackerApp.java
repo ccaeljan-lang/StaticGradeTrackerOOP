1e2df5aa8487b5a7667153978d525b610579d0d6
caeljandcristobal <ccaeljan@gmail.com>
2026-06-06 22:18:58 +0800

    [STATIC] FINAL FINISHED CODE.

--- src/ph/edu/dlsu/lbycpob/GradeTrackerApp.java
+++ src/ph/edu/dlsu/lbycpob/GradeTrackerApp.java
@@ -1,6 +1,11 @@
+// [IMPORTANT NOTE] CREATED STATIC FIRST AND THEN TURNED IT INTO NON-STATIC.
+// So the commit history
+// of the static code is part of the GradeTrackerOOP already.
+// Which finished at "FINISHED CODE" commit message.
+
 package ph.edu.dlsu.lbycpob;
 
-// ============================================================
+// ===========================================================
 // GradeTrackerApp.java
 // RESPONSIBILITY: Application entry point and menu controller
 // SRP: Orchestrates navigation between features — delegates all
